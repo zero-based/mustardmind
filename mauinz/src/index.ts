bdb13939ab1a658bc6e4e677fdf121bfae84d2f2
Michael Safwat Hanna <michaelsafwat.hanna@gmail.com>
2021-01-30 02:13:41 +0200

    chore(mauinz): configure package exports (#5)
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

---
+++ mauinz/src/index.ts
@@ -0,0 +1,5 @@
+export * from '@chakra-ui/react';
+export { theme } from './theme/theme';
+export { fonts } from './theme/font-face';
+export { Button } from './components/button/Button';
+export { Input } from './components/input/Input';
