14808b19d7dc9ac910684162c37f4395bb2ac540
Youssef Raafat <youssefraafatnasry@gmail.com>
2021-02-02 17:04:40 +0200

    feat(server): migrate to TypeGraphQL (#13)

--- packages/server/src/schema.ts
+++ packages/server/src/schema.ts
@@ -1,31 +1,13 @@
-import { makeSchema } from "nexus";
-import { nexusPrisma } from "nexus-plugin-prisma";
-import * as types from "./graphql";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
 import * as path from "path";
 
-export const schema = makeSchema({
-  types,
-  plugins: [nexusPrisma({ experimentalCRUD: true })],
-  outputs: {
-    typegen: path.join(
-      __dirname,
-      "../node_modules/@types/nexus-typegen/index.d.ts"
-    ),
-    schema: path.join(
-      __dirname,
-      "generated/schema.graphql"
-    ),
-  },
-  contextType: {
-    module: require.resolve("./context"),
-    export: "Context",
-  },
-  sourceTypes: {
-    modules: [
-      {
-        module: "@prisma/client",
-        alias: "prisma",
-      },
-    ],
-  },
-});
+const createSchema = async () => {
+  return await buildSchema({
+    resolvers: [path.join(__dirname, "graphql/*.resolver.[jt]s")],
+    emitSchemaFile: path.join(__dirname, "generated/schema.graphql"),
+    validate: false,
+  });
+};
+
+export const schema = createSchema();
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,8 +2,15 @@ import { ApolloServer } from "apollo-server";
 import { schema } from "./schema";
 import { createContext } from "./context";
 
-const server = new ApolloServer({ schema, context: createContext });
+const main = async () => {
+  const server = new ApolloServer({
+    schema: await schema,
+    context: createContext,
+  });
 
-server.listen().then(({ url }) => {
-  console.log(`⭐️ Server ready at: ${url}\n`);
-});
+  server.listen().then(({ url }) => {
+    console.log(`⭐️ Server ready at: ${url}\n`);
+  });
+};
+
+main().catch((err) => console.log(err));
